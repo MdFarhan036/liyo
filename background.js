4bfbf2100321bf0dc65fe1fed6a864f235b4a102
MdFarhan036 <82930880+MdFarhan036@users.noreply.github.com>
2022-10-31 01:10:59 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,63 @@
+const readLocalStorage = async(key) => {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get([key], function(result) {
+            if (result[key] === undefined) {
+                reject();
+            } else {
+                resolve(result[key]);
+            }
+        });
+    });
+};
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function makeLike(likeCount, content) {
+    for (let i = 0; i < likeCount; i++) {
+        const container = content[i].querySelector("div.feed-shared-social-actions.feed-shared-social-action-bar.social-detail-base-social-actions.feed-shared-social-action-bar--full-width.feed-shared-social-action-bar--has-social-counts > span.reactions-react-button.feed-shared-social-action-bar__action-button");
+        if (container) {
+            const temp = container.getElementsByTagName("button");
+            temp[0].click();
+        }
+        window.scrollBy(0, 1500);
+        await sleep(1000);
+    }
+}
+
+async function clickFunction() {
+    console.log("i am working");
+    const submitButton = document.querySelector("div.comments-comment-box__form-container.flex-grow-1 > form > div:nth-child(2)")
+    if (submitButton) {
+        console.log("working");
+        const btn = submitButton.getElementsByTagName("button");
+        btn[0].click();
+    }
+}
+async function makeComment(commentCount, content, message) {
+    if (content) {
+        for (let i = commentCount - 1; i >= 0; i--) {
+            const editBox = content[i].querySelector("div.comments-comment-box__form-container.flex-grow-1 > form > div > div > div.comments-comment-box-comment__text-editor > div > div.editor-container > div > div > div.ql-editor.ql-blank");
+            if (editBox) {
+                editBox.innerHTML = message;
+                setTimeout(clickFunction, 3000);
+            }
+            window.scrollBy(0, -1500);
+            console.log(i);
+            await sleep(2000);
+        }
+    }
+}
+async function myScript() {
+    const likeCount = await readLocalStorage('likeCount');
+    const commentCount = await readLocalStorage('commentCount');
+    const message = await readLocalStorage('message');
+    const content = document.querySelectorAll("#main > div:nth-child(3) > div > div.scaffold-finite-scroll__content:first-child > div");
+    console.log(content.length)
+    console.log(likeCount)
+    if (content) {
+        await makeLike(likeCount, content);
+        makeComment(commentCount, content, message);
+    }
+}
+window.addEventListener("load", myScript);
---
+++ popup.js
@@ -0,0 +1,29 @@
+document.getElementById("Open").addEventListener("click", async function() {
+    const link = "https://www.linkedin.com/feed/";
+    const likeCount = document.getElementById("likeCount").value;
+    const commentCount = document.getElementById("commentCount").value;
+    const message = document.getElementById("message").value;
+    if (likeCount && commentCount && message) {
+        if (likeCount > 0 && commentCount > 0) {
+            chrome.storage.local.set({ likeCount: likeCount, commentCount: commentCount, message: message });
+            chrome.tabs.create({
+                url: link
+            }, function(newTab) {
+                chrome.scripting.executeScript({
+                    target: { tabId: newTab.id },
+                    files: ['background.js'],
+                });
+            });
+        } else {
+            if (likeCount == 0 && commentCount == 0) {
+                alert("Please enter like count and comment count");
+            } else if (likeCount == 0) {
+                alert("Please enter valid like count");
+            } else {
+                alert("Please enter valid comment count");
+            }
+        }
+    } else {
+        window.alert("Please enter all the fields");
+    }
+});
